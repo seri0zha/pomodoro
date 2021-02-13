dcfbcba96f4e18e78a0d12b5c49a716081427f6f
seri0zha <ise200017@gmail.com>
2021-02-13 18:23:55 +0300

    Added background change on time switch

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,6 +4,9 @@ import Header from "../Header/Header";
 import Pomodoro from "../Pomodoro/Pomodoro";
 import Tasks from "../Tasks/Tasks";
 import pomodoroStore from "../../Stores/PomodoroStore";
+import {Helmet} from "react-helmet";
+import {observer} from "mobx-react";
+
 
 const handleKeyPress = (e: KeyboardEvent) => {
   const {keyCode} = e;
@@ -16,7 +19,18 @@ const handleKeyPress = (e: KeyboardEvent) => {
   }
 }
 
-const App = () => {
+const getColor = (defaultTime: number): string => {
+  switch (defaultTime) {
+    case 5*60*1000:
+      return "rgba(115,160,110,0.8)";
+    case 15*60*1000:
+      return 'rgba(102,150,150,0.8)';
+    default:
+      return 'rgba(130,30,18,0.8)';
+  }
+}
+
+const App = observer(() => {
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
     return () => {
@@ -25,12 +39,22 @@ const App = () => {
   }, [handleKeyPress]);
 
   return (
-    <div className={styles.App}>
-      <Header />
-      <Pomodoro />
-      <Tasks />
-    </div>
+    <>
+      <Helmet>
+        <style>
+          {`body {
+              background-color: ${getColor(pomodoroStore.defaultCountdownTime)};
+              transition: background-color 500ms ease-out;
+          }`}
+        </style>
+      </Helmet>
+      <div className={styles.App}>
+        <Header />
+        <Pomodoro />
+        <Tasks />
+      </div>
+    </>
   );
-}
+});
 
 export default App;
