0789f11e2578292fda59bebd7c114cf646849e33
seri0zha <ise200017@gmail.com>
2021-02-13 17:39:17 +0300

    Added a listener for space press

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './App.module.css';
 import Header from "../Header/Header";
 import Pomodoro from "../Pomodoro/Pomodoro";
 import Tasks from "../Tasks/Tasks";
-function App() {
+import pomodoroStore from "../../Stores/PomodoroStore";
+
+const handleKeyPress = (e: KeyboardEvent) => {
+  const {keyCode} = e;
+  if (keyCode === 32) {
+    if (pomodoroStore.timerIsRunning) {
+      pomodoroStore.stopTimer();
+    } else {
+      pomodoroStore.startTimer();
+    }
+  }
+}
+
+const App = () => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
   return (
     <div className={styles.App}>
       <Header />
